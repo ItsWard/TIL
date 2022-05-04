ee1134ebe16cb8e966caf39edc40fdf0171b72fd
ward <bonuswon@gmail.com>
2022-05-04 22:24:44 +0900

    add java

--- JAVA/JAVA-BASIC/Hello-World/src/Main.java
+++ JAVA/JAVA-BASIC/Hello-World/src/Main.java
@@ -1,2 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    String inputValue = scanner.nextLine();
+    System.out.println(inputValue);
+    }
 }
