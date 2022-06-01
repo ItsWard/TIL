4c4782393aa439b96648d78031cbbdcbb9b6cff5
ward <bonuswon@gmail.com>
2022-06-01 18:17:33 +0900

    Add jsp

---
+++ spring-boot/http-rest/src/main/java/com/example/httprest/controller/HttpRespController.java
@@ -0,0 +1,32 @@
+package com.example.httprest.controller;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HttpRespController {
+
+    @GetMapping("/txt")
+    public String txt() {
+
+        return "demo.txt";
+    }
+    
+    @GetMapping("/html")
+    public String mus() {
+        
+        return "index";
+    }
+
+    @GetMapping("/jsp")
+    public String jsp() {
+
+        return "JSPdemo";
+    }
+
+    
+
+
+}
