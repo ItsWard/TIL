feb4adebc9616106d21b1ce4bd4bee1d74a1595b
ward <bonuswon@gmail.com>
2022-06-01 21:25:40 +0900

    Add spring-java-jsp

--- spring-boot/http-rest/src/main/java/com/example/httprest/controller/JavaToController.java
+++ spring-boot/http-rest/src/main/java/com/example/httprest/controller/JavaToController.java
@@ -1,6 +1,8 @@
 package com.example.httprest.controller;
 
+import com.example.httprest.domain.User;
 import org.springframework.stereotype.*;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,5 +13,17 @@ public class JavaToController {
 
         return "jspdemo2";
     }
-    
+
+    @GetMapping("/jsp/java/model")
+    public String jspToJavaToModel(Model model) { //메소드의 파라미터에 Model을 선언하고,
+
+        User user = new User();
+        user.setUsername("ward");
+
+        model.addAttribute("username", user.getUsername()); //addAttribute메소드로 전달하면됨 .
+
+        return "jspdemo3";
+    }
+
+
 }
---
+++ spring-boot/http-rest/src/main/webapp/WEB-INF/views/jspdemo3.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 이원용
+  Date: 2022-06-01
+  Time: 오후 9:19
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>This is demo3</h1>
+<h3>${username}</h3>
+</body>
+</html>
