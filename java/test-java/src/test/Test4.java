0b1b837d80152049e103136c2a9118a0cdc58f29
ward <bonuswon@gmail.com>
2022-07-16 23:52:48 +0900

    TIL

--- java/test-java/src/test/Test4.java
+++ java/test-java/src/test/Test4.java
@@ -1,46 +1,61 @@
 package test;
 
-import java.util.Arrays;
-import java.util.function.Predicate;
+import java.util.*;
+import java.io.*;
+
 
 public class Test4 {
 
-    public static void main(String[] args) {
-        int b = 1000;
-        long a;
-        a = b;
 
-        int[] base = new int[50001];
-        for(int i = 0 ; i < 50001 ; i++)
-            base[i] = i;
-
-        int[] sample = new int[50001];
-        for(int i = 0 ; i < 50001 ; i++)
-            sample[i] = i;
-
-        long beforeTime = System.currentTimeMillis();
-
-        System.out.println(isSubsetOf2(base, sample));
-
-        long afterTime = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
-        long secDiffTime = (afterTime - beforeTime);//두 시간에 차 계산
-        System.out.println("시간차이(u) : "+secDiffTime);
-    }
+    public static void main(String[] args) throws IOException{
 
 
-    public static boolean isSubsetOf(int[] base, int[] sample) {
-        // TODO:
-        return Arrays.stream(sample)
-                .allMatch( i -> Arrays.stream(base)
-                        .anyMatch(j -> i == j));
+        System.out.println(getMaxMeeting());
+
 
     }
 
-    public static boolean isSubsetOf2(int[] base, int[] sample) {
-        Arrays.sort(base);  // binarySearch을 사용하기 전 사전작업
-        for(int i : sample)
-            if(Arrays.binarySearch(base, i) < 0)  // binarySearch의 반환값 : 존재하는 경우는 index 반환 / 존재하지 않으면 음수값 반환
-                return false;
-        return true;
+    public static int getMaxMeeting () throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int max = Integer.parseInt(br.readLine());
+
+        int[][] time = new int[max][2];
+        String input;
+
+        StringTokenizer st;
+
+        for(int i = 0 ; i < max ; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(time, new Comparator<int[]>() {
+
+            @Override
+            public int compare(int[] o1, int[] o2) {
+
+                if(o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+
+                return o1[1] - o2[1];
+            }
+
+        });
+
+        int count = 0;
+        int meetTime = 0;
+
+        for (int i = 0; i < max; i++) {
+            if (meetTime < time[i][0]) {
+                meetTime = time[i][1];
+                count++;
+            }
+        }
+
+        return count;
     }
+
 }
